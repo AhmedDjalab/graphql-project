a2a72797f08cd4cd4044c7d6e64fd7e412f2de4d
Ahmed <ahmedlight@yandex.com>
2022-08-14 12:27:02 +0100

    init project

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+// Construct a schem,a using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+app.listen(port, () =>
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql")
+);
